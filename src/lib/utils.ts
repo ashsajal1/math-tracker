701ee3ae31efb4ab865dbba3442c5bda6e184200
Ashsajal <ashsajal1@gmail.com>
2025-08-31 22:13:03 +0600

    feat: add capitalize

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function capitalize(str: string) {
+  return str.split(/\s+/).map(word => {
+    return word ? word[0].toUpperCase() + word.slice(1).toLowerCase() : '';
+  }).join(' ');
+}
